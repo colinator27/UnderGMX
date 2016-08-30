7c32ee3436bfbd134d2051b0fdeb51e148a606d5
colinator27 <colinpace13@gmail.com>
2016-08-30 10:34:09 -0400

    Support for scripts.

--- UnderGMX/Scripts.cs
+++ UnderGMX/Scripts.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,39 @@ namespace UnderGMX
 {
     class Scripts
     {
+        public static void writeScripts(string appdirectory, string[] scrlist)
+        {
+            int i = 0;
+            int i2 = 0;
+            int length = scrlist.Length;
+            string[] filecontent;
+            string filecontentstr;
+            while(i < length)
+            {
+                Console.WriteLine("Writing script: " + scrlist[i]);
+                filecontent = File.ReadAllLines(@appdirectory + "scripts\\" + scrlist[i] + ".js");
+                i2 = 0;
+                filecontentstr = "";
+                bool foundbend = false;
+                while (i2 < filecontent.Length)
+                {
+                    if(foundbend == true) filecontentstr += (filecontent[i2] + Environment.NewLine);
+                    if(foundbend == false)
+                    {
+                        if(filecontent[i2] == "*/")
+                        {
+                            foundbend = true;
+                        }
+                    }
+                    i2++;
+                }
+                using (var dest = File.AppendText(Path.Combine(@appdirectory + "UnderGMX.gmx\\scripts\\", scrlist[i] + ".gml")))
+                {
+                    dest.Write(filecontentstr);
+                }
+                i++;
+            }
+        }
         public static String[] getScriptDataNames(string appdirectory)
         {
             DirectoryInfo d = new DirectoryInfo(@appdirectory + "scripts\\");
